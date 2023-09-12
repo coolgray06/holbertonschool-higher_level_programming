2d1f101b363c66579de8698a5e4a362645072722
coolgray06 <6636@holbertonstudents.com>
2023-09-12 02:49:09 -0700

    new element 2

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@ def new_in_list(my_list, idx, element):
     my_length = len(my_list)
     new_in_list = my_list.copy()
 
-if 0 <= idx or < my_length:
+    if 0 > idx or idx >= my_length:
+        return new_in_list
     new_in_list[idx] = element
-    return(new_in_list)
-else:
-    return(my_list)
+    return new_in_list
+   
