183ef2544f2932e4f937659bcdf1cf3e58d665da
coolgray06 <6636@holbertonstudents.com>
2023-09-12 10:47:32 -0700

    double the matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda x: [i**2 for i in x], matrix))
+    new_matrix = []
+    for row in matrix:
+        new_matrix_row = []
+        for element in row:
+            new_matrix_row.append(element**2)
+        new_matrix.append(new_matrix_row)
     return(new_matrix)
