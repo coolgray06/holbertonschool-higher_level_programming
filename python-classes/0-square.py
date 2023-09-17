315bfbb2488dcca2b3733768787656d79d454011
coolgray06 <6636@holbertonstudents.com>
2023-09-17 11:01:13 -0700

    pass class 3

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" 
+
+"""
 Module doc: This module creates an empty class Square that defines a square.
 """
+
 class Square:
+
     """
     Class doc: This is an empty class called Square
     """
