4955b1bea5e146f4d764780859a96edcccf06693
coolgray06 <6636@holbertonstudents.com>
2023-09-17 11:32:47 -0700

    making room

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module doc: Ths module takes the size of the square
+as an argument and initializes the __size attribute.
+"""
+
+
+class Square:
+
+    """
+    Class doc: This class has a private instance attribute called __size,
+    which represents the size of the square.
+    """
+    def __init__(self, size):
+        self.__size = size
