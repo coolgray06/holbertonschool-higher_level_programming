03a3c662d72e4b3eee77cc28d4293278d5f51f2d
coolgray06 <6636@holbertonstudents.com>
2023-09-19 13:27:33 -0700

    access and update

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-
 """
 Module doc: This module checks if the size argument is an integer,
 and raises a TypeError exception if it is not.
 It also checks if the size argument is less than 0,
-and raises a ValueError exception if it is. The area method will
-return the current square area.
+and raises a ValueError exception if it is.
 """
 
 
 class Square:
+
     """
     Class doc: Represents a square.
     """
@@ -17,12 +16,27 @@ class Square:
         """
         Initializes a new square.
         """
-
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         self.__size = size
+
+    def size(self):
+        """
+        Retrieves and returns the size of the square.
+        """
+        return self.__size
+
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value: The new size of the square.
+            Must be an integer greater than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """
