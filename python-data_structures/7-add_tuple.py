90dfe96c2b1248943cd79000b34b61433171808a
coolgray06 <6636@holbertonstudents.com>
2023-09-20 15:58:08 -0700

    tweaked return

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,7 +5,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
     tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
 
-    mytup_a = tuple_a
-    mytup_b = tuple_b
-
-    return (mytup_a, mytup_b)
+    return tuple(a + b for a, b in zip(tuple_a[:2], tuple_b[:2]))
